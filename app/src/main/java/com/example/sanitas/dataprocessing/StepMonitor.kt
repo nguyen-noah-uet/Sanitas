b6c0481ad88efaf2d562765d54155ceee20e12ea
dhtruongvnu <dhtruongvnu@gmail.com>
2023-12-25 01:48:35 +0700

    update dynamic numberSample for detecting steps using low pass filter

--- app/src/main/java/com/example/sanitas/dataprocessing/StepMonitor.kt
+++ app/src/main/java/com/example/sanitas/dataprocessing/StepMonitor.kt
@@ -3,6 +3,125 @@ package com.example.sanitas.dataprocessing
 import android.os.Build
 import androidx.annotation.RequiresApi
 import com.example.sanitas.SanitasApp
+import kotlin.math.pow
+import kotlin.math.sqrt
+
+class StepCounter {
+    private var filter = LowPassFilter()
+
+    private var dynamicNumberSample = 10.0
+    private var minNumberSample = 3
+    private var maxNumberSample = 20
+
+    private var onTop = false
+    private var onBottom = false
+
+    private var currentAcc = 9.8
+    private var dynamicUpThreshold = 10.5
+    private var dynamicDownThreshold = 8.7
+    private var defaultUpThreshold = 10.5
+    private var defaultDownThreshold = 8.7
+    private var reductionRateValue = 0.02
+    private var raiseRateValue = 0.02
+
+    private var inRest = false
+    public fun updateThreshold() {
+        if (currentAcc > dynamicUpThreshold) {
+            // Update threshold if va is greater than the current threshold
+            dynamicNumberSample -= 0.3
+            dynamicUpThreshold = currentAcc
+            reductionRateValue = (currentAcc - defaultUpThreshold) / maxNumberSample
+        } else if (dynamicUpThreshold > defaultUpThreshold) {
+            // Reduce threshold if it's greater than the default threshold
+            dynamicUpThreshold -= reductionRateValue
+        } else {
+            // Reset threshold to default value
+            dynamicUpThreshold = defaultUpThreshold
+        }
+
+        if (currentAcc < dynamicDownThreshold) {
+            dynamicNumberSample -= 0.3
+            dynamicDownThreshold = currentAcc
+            raiseRateValue = (defaultDownThreshold - currentAcc) / maxNumberSample
+        } else if (dynamicDownThreshold < defaultUpThreshold) {
+            dynamicDownThreshold += reductionRateValue
+        } else {
+            dynamicDownThreshold = defaultUpThreshold
+        }
+
+        if (kotlin.math.abs(currentAcc - 9.8) < 1) {
+            dynamicNumberSample += 0.3
+            if (dynamicNumberSample > maxNumberSample) {
+                inRest = true
+            }
+        }
+
+        if (dynamicNumberSample > maxNumberSample) {
+            dynamicNumberSample = maxNumberSample * 1.0
+        } else if (dynamicNumberSample < minNumberSample) {
+            dynamicNumberSample = minNumberSample * 1.0
+        }
+
+    }
+
+    public fun filteredResult(input: Double): Double{
+        currentAcc = this.filter.filteredResult(input)
+        return currentAcc
+    }
+
+    private fun isTop(): Boolean {
+        val filteredOutput = filter.getFilteredOutput()
+        if(filteredOutput[dynamicNumberSample.toInt()] < dynamicUpThreshold) {
+            return false
+        }
+        for (i in 0 until dynamicNumberSample.toInt()) {
+            if (filteredOutput[i] + 0.04 >= filteredOutput[i + 1]) {
+                return false
+            }
+        }
+        for (i in dynamicNumberSample.toInt() until 2*dynamicNumberSample.toInt()) {
+            if (filteredOutput[i] <= filteredOutput[i + 1] + 0.04) {
+                return false
+            }
+        }
+        return true
+    }
+
+    private fun isBot(): Boolean {
+        val filteredOutput = filter.getFilteredOutput()
+        if(filteredOutput[dynamicNumberSample.toInt()] > dynamicDownThreshold) {
+            return false
+        }
+        for (i in 0 until dynamicNumberSample.toInt()) {
+            if (filteredOutput[i + 1] + 0.01 >= filteredOutput[i]) {
+                return false
+            }
+        }
+        for (i in dynamicNumberSample.toInt() until 2*dynamicNumberSample.toInt()) {
+            if (filteredOutput[i + 1] <= filteredOutput[i] + 0.01) {
+                return false
+            }
+        }
+//        var str = ""
+//        for (i in 0 until 2*minNumberSample){
+//            str += filteredOutput[i].toString() + " "
+//        }
+        return true
+
+    }
+
+    public fun checkStep(): Boolean {
+        onTop = onTop or isTop()
+        onBottom = onBottom or isBot()
+        if(onTop and onBottom) {
+            onTop = false
+            onBottom = false
+            return true
+        }
+        return false
+    }
+
+}
 
 class StepMonitor private constructor() {
     private object Holder { val INSTANCE = StepMonitor() }
@@ -13,6 +132,7 @@ class StepMonitor private constructor() {
             return Holder.INSTANCE
         }
     }
+    private var stepCounter = StepCounter()
     private var onStepDetected: () -> Unit = {}
     private var dynamicThreshold = 0.05
     private var defaultThreshold = 0.05
@@ -33,6 +153,8 @@ class StepMonitor private constructor() {
     private var rawAz = 0.0f
     private var rawRoll = 0.0f
     private var rawPitch = 0.0f
+
+    private var useLowPassFilter = true
 
     fun setOnStepDetectedCallback(onStepDetected: () -> Unit) {
         this.onStepDetected = onStepDetected
@@ -76,49 +198,67 @@ class StepMonitor private constructor() {
 
     @RequiresApi(Build.VERSION_CODES.O)
     public fun detectStep() : Boolean {
-        // Convert raw rotation to degrees
-        val roll = rawRoll * 180 / Math.PI
-        val pitch = rawPitch * 180 / Math.PI
-
-        // Rotation rate normalization
-        val roll90 = if (roll > 90) 90.0 - (roll - 90.0)
-        else if (roll < -90) -90.0 - (roll + 90.0)
-        else roll
-
-        val pitch90 = pitch
-
-        // Rotation rate normalization (-1.0 to 1.0)
-        val rollNormalize = roll90 / 90
-        val pitchNormalize = pitch90 / 90
-
-        // Calculate the weight of components Ax, Ay, Az
-        val xw = rollNormalize * (1.0 - kotlin.math.abs(pitchNormalize))
-        val yw = -1.0 * pitchNormalize
-        val zw = if (kotlin.math.abs(roll) > 90) 1.0 - (kotlin.math.abs(xw) + kotlin.math.abs(yw))
-        else -1.0 * (1.0 - (kotlin.math.abs(xw) + kotlin.math.abs(yw)))
-
-        // Calculate vertical acceleration
-        predVa = rawAx * xw + rawAy * yw + rawAz * zw
-
-        // Smooth vertical acceleration
-        verticalAcc = predVa * 0.1 + verticalAcc * 0.9
-
-        // Step detection
-        isStep = false
-        if (verticalAcc > dynamicThreshold && stepCountFlag && frameCount > frameCountMin) {
-            stepCountFlag = false
-            frameCount = 0
-            isStep = true
-            SanitasApp.currentSteps += 1
-            if(onStepDetected != null) {
-                onStepDetected()
+        if (useLowPassFilter) {
+            val totalAcc = sqrt(this.rawAx.toDouble().pow(2.0) + this.rawAy.toDouble().pow(2.0) + this.rawAz.toDouble()
+                .pow(2.0)
+            )
+            stepCounter.filteredResult(totalAcc)
+            var isStep = false
+            if (stepCounter.checkStep()) {
+                SanitasApp.currentSteps += 1
+                if(onStepDetected != null) {
+                    onStepDetected()
+                }
+                isStep = true
             }
-        } else if (verticalAcc < 0.0) {
-            stepCountFlag = true
+            stepCounter.updateThreshold()
+            return isStep
+        } else {
+            // Convert raw rotation to degrees
+            val roll = rawRoll * 180 / Math.PI
+            val pitch = rawPitch * 180 / Math.PI
+
+            // Rotation rate normalization
+            val roll90 = if (roll > 90) 90.0 - (roll - 90.0)
+            else if (roll < -90) -90.0 - (roll + 90.0)
+            else roll
+
+            val pitch90 = pitch
+
+            // Rotation rate normalization (-1.0 to 1.0)
+            val rollNormalize = roll90 / 90
+            val pitchNormalize = pitch90 / 90
+
+            // Calculate the weight of components Ax, Ay, Az
+            val xw = rollNormalize * (1.0 - kotlin.math.abs(pitchNormalize))
+            val yw = -1.0 * pitchNormalize
+            val zw = if (kotlin.math.abs(roll) > 90) 1.0 - (kotlin.math.abs(xw) + kotlin.math.abs(yw))
+            else -1.0 * (1.0 - (kotlin.math.abs(xw) + kotlin.math.abs(yw)))
+
+            // Calculate vertical acceleration
+            predVa = rawAx * xw + rawAy * yw + rawAz * zw
+
+            // Smooth vertical acceleration
+            verticalAcc = predVa * 0.1 + verticalAcc * 0.9
+
+            // Step detection
+            isStep = false
+            if (verticalAcc > dynamicThreshold && stepCountFlag && frameCount > frameCountMin) {
+                stepCountFlag = false
+                frameCount = 0
+                isStep = true
+                SanitasApp.currentSteps += 1
+                if(onStepDetected != null) {
+                    onStepDetected()
+                }
+            } else if (verticalAcc < 0.0) {
+                stepCountFlag = true
+            }
+            frameCount++
+            updateThreshold()
+            return isStep
         }
-        frameCount++
-        updateThreshold()
-        return isStep
+
     }
 
 }
--- app/src/main/java/com/example/sanitas/dataprocessing/filter.kt
+++ app/src/main/java/com/example/sanitas/dataprocessing/filter.kt
@@ -1,94 +1,43 @@
 package com.example.sanitas.dataprocessing
-import android.util.Log
 import kotlin.math.PI
 import kotlin.math.sin
 import kotlin.random.Random.Default.nextInt
 
-// butterworth 4th, wc = 4/(50/2) (fc/(fs/2))
-val bCoefficient = listOf(0.00048944, 0.0024, 0.0049, 0.0049, 0.0024, 0.00048944)
-val aCoefficient = listOf(1.0, -3.378, 4.7518, -3.4397, 1.2740, -0.1924)
-val sizeFilter = bCoefficient.size
-const val saveLength = 100
-val filteredOutput = MutableList(saveLength) { 9.8 }
-val currentInput = MutableList(sizeFilter) { 9.8 }
+class LowPassFilter {
+    // butterworth 4th, wc = 4/(50/2) (fc/(fs/2))
+    private val bCoefficient = listOf(0.00048944, 0.0024, 0.0049, 0.0049, 0.0024, 0.00048944)
+    private val aCoefficient = listOf(1.0, -3.378, 4.7518, -3.4397, 1.2740, -0.1924)
+    private val sizeFilter = bCoefficient.size
+    private val saveLength = 100
+    private val currentInput = MutableList(sizeFilter) { 9.8 }
+    private val filteredOutput = MutableList(saveLength) { 9.8 }
 
-const val minNumberSample = 6
-var onTop = false
-var onBottom = false
-val minTop = 11
-val maxBot = 8.7
+    public fun getFilteredOutput(): MutableList<Double> {
+        return this.filteredOutput
+    }
 
-fun filteredResult(input: Double): Double {
-    filteredOutput.removeAt(saveLength - 1)
-    currentInput.removeAt(sizeFilter - 1)
-    currentInput.add(0, input)
-    val output = (bCoefficient.zip(currentInput) { b, c -> b * c }.sum()) - (filteredOutput.subList(0, sizeFilter - 1).zip(aCoefficient.subList(1, sizeFilter)) { f, a -> f * a }.sum())
-    filteredOutput.add(0, output)
-    return output
-}
-
-fun isTop(): Boolean {
-    if(filteredOutput[minNumberSample] < minTop) {
-        return false
+    public fun filteredResult(input: Double): Double {
+        filteredOutput.removeAt(saveLength - 1)
+        currentInput.removeAt(sizeFilter - 1)
+        currentInput.add(0, input)
+        val output = (bCoefficient.zip(currentInput) { b, c -> b * c }.sum()) - (filteredOutput.subList(0, sizeFilter - 1).zip(aCoefficient.subList(1, sizeFilter)) { f, a -> f * a }.sum())
+        filteredOutput.add(0, output)
+        return output
     }
-    for (i in 0 until minNumberSample) {
-        if (filteredOutput[i] + 0.04 >= filteredOutput[i + 1]) {
-            return false
-        }
-    }
-    for (i in minNumberSample until 2*minNumberSample) {
-        if (filteredOutput[i] <= filteredOutput[i + 1] + 0.04) {
-            return false
-        }
-    }
-    return true
-}
-
-fun isBot(): Boolean {
-    if(filteredOutput[minNumberSample] > maxBot) {
-        return false
-    }
-    for (i in 0 until minNumberSample) {
-        if (filteredOutput[i + 1] + 0.01 >= filteredOutput[i]) {
-            return false
-        }
-    }
-    for (i in minNumberSample until 2*minNumberSample) {
-        if (filteredOutput[i + 1] <= filteredOutput[i] + 0.01) {
-            return false
-        }
-    }
-    var str = ""
-    for (i in 0 until 2*minNumberSample){
-        str += filteredOutput[i].toString() + " "
-    }
-    return true
-
-}
-
-fun checkStep(): Boolean {
-    onTop = onTop or isTop()
-    onBottom = onBottom or isBot()
-    if(onTop and onBottom) {
-        onTop = false
-        onBottom = false
-        return true
-    }
-    return false
 }
 
 fun main() {
+    val counter = StepCounter()
     val nSamples = 200
     val fs = 50
     val input = (0 until nSamples).map { i ->
-        5 * sin((2 * PI * 5 * i) / fs) + nextInt(0, 100)/50
+        35 * sin((2 * PI * 3 * i) / fs) + nextInt(0, 100)/50
     }
     val output = mutableListOf<Double>()
     var count = 0
     for (i in 0 until nSamples) {
-//        output.add(filteredResult(filteredOutput, input[i]))
-        filteredResult(input[i])
-        if(checkStep()) {
+        output.add(counter.filteredResult(input[i]))
+        if(counter.checkStep()) {
             count += 1
             println(count)
         }
